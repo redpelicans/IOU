5550128259c73f1765ce77cea7fdb4faa6bcaa5a
Thomas Tridon <thomastridon@MacBook-Pro-de-Pauline.local>
2018-04-30 17:14:20 +0200

    I001 Create Events

---
+++ src/client/web/components/Events/Preview.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+import Card, {
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+} from 'material-ui/Card';
+import Typography from 'material-ui/Typography';
+
+const StyledCard = styled(Card)`
+  max-width: 400px;
+`;
+
+const Preview = () => {
+  return (
+    <StyledCard>
+      <CardHeader avatar="R" subheader="21 Jan. 2017" title="Event" />
+      <CardMedia image="" title="event" />
+      <CardContent>
+        <Typography>Description</Typography>
+      </CardContent>
+      <CardContent>Members</CardContent>
+    </StyledCard>
+  );
+};
+
+export default Preview;
---
+++ src/client/web/components/Events/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Preview from './Preview';
+
+const Events = () => {
+  return <Preview />;
+};
+
+export default Events;
