e15d591681c06fcc73e5fa60f7d0e464217a8bfc
Thomas Tridon <thomastridon@MacBook-Pro-de-Pauline.local>
2018-04-30 16:18:02 +0200

    I001 Add Header

---
+++ src/client/web/components/App/header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import AppBar from 'material-ui/AppBar';
+import IconButton from 'material-ui/IconButton';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import History from '@material-ui/icons/History';
+
+const StyledTypography = styled(Typography)`
+  flex: 1;
+`;
+
+const Header = () => {
+  return (
+    <AppBar>
+      <Toolbar>
+        <StyledTypography color="inherit" variant="title">
+          IOU by RedPelicans
+        </StyledTypography>
+        <IconButton color="inherit">
+          <History />
+        </IconButton>
+        <IconButton color="inherit">
+          <AccountCircle />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
