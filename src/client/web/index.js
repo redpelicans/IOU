9b8b68f316d025c6eb88d4ba5c4cbd536539404a
Thomas TRIDON <ttridon@e1r2p3.42.fr>
2018-05-01 18:43:03 +0200

    I001 Implemente mock data

--- src/client/web/index.js
+++ src/client/web/index.js
@@ -1,5 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import createLogger from 'redux-logger';
+import thunk from 'redux-thunk';
+import reducer from './reducers';
+import { loadEvents } from './actions/events';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('__IOU__'));
+const store = createStore(reducer, applyMiddleware(createLogger, thunk));
+
+store.dispatch(loadEvents());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('__IOU__'),
+);
