baf4b1b454879fe0fd0efbdce4f0f254f78f060d
Eric <eric.basley@redpelicans.com>
2018-05-07 09:29:25 +0200

    Add events endpoints
    * event:add
    * event:update
    * event:get

--- src/mock/api.vibes.js
+++ src/mock/api.vibes.js
@@ -1,5 +1,6 @@
 const { vibe } = require('farso');
-const { path } = require('ramda');
+const faker = require('faker');
+const { findIndex, propEq, find, path } = require('ramda');
 
 vibe.default(
   'Main',
@@ -15,8 +16,31 @@ vibe.default(
       },
     },
   ) => {
-    mock('events').reply([200, events]);
-
-    mock('people').reply([200, people]);
+    mock('events:list').reply([200, events]);
+    mock('people:list').reply([200, people]);
+    mock('event:add').reply((req, res) => {
+      const id = faker.random.uuid();
+      const newEvent = {
+        attendeeIds: [],
+        currency: 'EUR',
+        ...req.body,
+        id,
+        createdAt: new Date(),
+      };
+      events.push(newEvent);
+      res.json(newEvent);
+    });
+    mock('event:update').reply((req, res) => {
+      const updates = req.body;
+      const index = findIndex(propEq('id', req.params.id), events);
+      if (index === -1) return res.sendStatus(404);
+      const updatedEvent = { ...events[index], ...updates };
+      events[index] = updatedEvent;
+      res.json(updatedEvent);
+    });
+    mock('event:get').reply((req, res) => {
+      const { id } = req.params;
+      res.json(find(propEq('id', id), events));
+    });
   },
 );
--- src/mock/endpoints.js
+++ src/mock/endpoints.js
@@ -1,4 +1,7 @@
 const { endpoint } = require('farso');
 
-endpoint('events', { uri: '/api/events', method: 'get' });
-endpoint('people', { uri: '/api/people', method: 'get' });
+endpoint('events:list', { uri: '/api/events', method: 'get' });
+endpoint('event:add', { uri: '/api/events', method: 'post' });
+endpoint('event:update', { uri: '/api/events/:id', method: 'patch' });
+endpoint('event:get', { uri: '/api/events/:id', method: 'get' });
+endpoint('people:list', { uri: '/api/people', method: 'get' });
